b34ba634b16ab156ed133c24b8b5d11672f85c0e
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2025-06-16 12:00:57 +0200

    feat: server config and DB creation

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+async function connectDB() {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to the database')
+}
+
+
+//Server init
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server listening correctly in port ${PORT}`)
+})
---
+++ models/movies.model.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require('mongoose');
+
+const movieSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    year: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    description: {
+        type: String,
+        required: false,
+        unique: false,
+    },
+    imdb_Rating: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    genre: {
+        type: [String],
+        required: false,
+        enum: [
+            "Action",
+            "Adult",
+            "Adventure",
+            "Animation",
+            "Biography",
+            "Comedy",
+            "Crime",
+            "Documentary",
+            "Drama",
+            "Family",
+            "Fantasy",
+            "Film-Noir",
+            "Game-Show",
+            "History",
+            "Horror",
+            "Music",
+            "Musical",
+            "Mystery",
+            "News",
+            "Reality-TV",
+            "Romance",
+            "Sci‑Fi",
+            "Short",
+            "Sport",
+            "Talk‑Show",
+            "Thriller",
+            "War",
+            "Western",
+            "Other"
+        ]
+    },
+    is_watched: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+const Movie = model('Novie', movieSchema);
+
+module.exports = Movie;
